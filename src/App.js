da81dd77d935f971640de88ae88b0cd019e5d9f8
jeremytjimenez <jeremytjimenez@gmail.com>
2023-08-16 14:54:23 -0400

    added file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from './components/About/About'
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/info" element={<Info />}/>
+          <Route path="/games" element={<AllGames />} />
+          <Route path="/games/create-game" element={<NewGame />} />
+          <Route path="/games/:id" element={<Game />} />
+          <Route path="/games/:id/edit" element={<EditGame />} />
+          <Route path="/404" element={<h1>404 Not found!</h1>} />
+          <Route path="*" element={<h1>404 Not found!</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Api/API.js
@@ -0,0 +1,45 @@
+import Axios from "./Axios";
+
+async function createGame(data) {
+  try {
+    let result = await Axios.post(`/games`, data);
+    return result;
+  } catch (error) {
+    alert(error.response.data.error);
+    return error;
+  }
+}
+
+async function handleDeleteById(id) {
+  try {
+    let result = await Axios.delete(`/games/${id}`);
+    alert("Successfully deleted!");
+    return result
+  } catch (error) {
+    console.log(error);
+    return error
+  }
+}
+
+async function getAllGames() {
+  try {
+    let result = await Axios.get("/games");
+
+    return result;
+  } catch (e) {
+    return e;
+  }
+};
+
+async function updateGameById(id, data) {
+  try {
+    let result = await Axios.put(`/games/${id}`, data);
+    return result;
+  } catch (error) {
+    alert(error.response.data.error);
+    return error;
+  }
+}
+
+export { createGame, getAllGames, updateGameById, handleDeleteById };
+
