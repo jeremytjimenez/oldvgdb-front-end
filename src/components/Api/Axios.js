9d1e4f1a6c09d6f04dccbed13d1394f3ad842cf9
jeremytjimenez <jeremytjimenez@gmail.com>
2023-08-21 01:37:53 -0400

    added backend deployment url

--- src/components/Api/Axios.js
+++ src/components/Api/Axios.js
@@ -4,7 +4,7 @@ const AxiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3001"
-      : "https://overpriced-backend.onrender.com",
+      : "https://oldvgdb-back-end.onrender.com",
   timeout: 50000,
 });
 
