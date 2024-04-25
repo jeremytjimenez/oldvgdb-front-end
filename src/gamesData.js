093375198c550c77a912f552018cb7237f0af037
Jeremy Jimenez <104604089+jeremytjimenez@users.noreply.github.com>
2024-04-25 15:11:37 -0400

    Create gamesData.js

---
+++ src/gamesData.js
@@ -0,0 +1,43 @@
+let gamesData = [
+    {
+        id: 1,
+        title: 'Darklands',
+        release_year: '1990',
+        developer: 'MPSLabs',
+        publisher: 'MicroProse',
+        country: 'United States of America',
+        platform: 'PC',
+        genre: 'RPG',
+        player_count: 1,
+        art: 'https://classicreload.com/sites/default/files/darklands-screenshot.png',
+        description: 'You control a party of adventurers during the reign of the Holy Roman Empire during the 15th century, while fighting various supernatural dangers.'
+    },
+    {
+        id: 2,
+        title: "King's Quest IV",
+        release_year: '1988',
+        developer: 'Sierra On-Line',
+        publisher: 'Sierra On-Line',
+        country: 'United States of America',
+        platform: 'PC',
+        genre: 'Graphic Adventure',
+        player_count: 1,
+        art: 'https://upload.wikimedia.org/wikipedia/en/2/2b/King%27s_Quest_IV_-_The_Perils_of_Rosella_Coverart.jpg',
+        description: 'You take on the role of Princess Rosella and must save her father and a good fairy from an evil witch.'
+    },
+    {
+        id: 3,
+        title: 'Fallout',
+        release_year: '1997',
+        developer: 'Interplay Productions',
+        publisher: 'Interplay Productions',
+        country: 'United States of America',
+        platform: 'PC',
+        genre: 'Isometric RPG',
+        player_count: 1,
+        art: 'https://media.moddb.com/images/games/1/1/91/screenshot9.jpg',
+        description: "You play as a post-apocalyptic survivor residing in the safety of one of many vaults created to withstand the nuclear fallout initiated many years ago. However, your vault's water chip is on its last legs, so you must go out into the wasteland and find a new one to save your people, encountering many dangers in the new and scary wasteland."
+    }
+];
+
+module.exports = gamesData;
